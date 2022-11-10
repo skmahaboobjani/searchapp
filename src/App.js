94555f539fc685e44f5321e72a50cf1306d3785d
mahaboob jani <skmabujani521@gmail.com>
2022-11-10 12:18:16 +0530

    mahaboob

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+ import Products from "./components/Products";
+const App = () => {
+ 
+  const [search,setSearch] = useState('');
+  const [data, setData] = useState([]);
+  const [clicked, setclicked] = useState(false);
+  const YOUR_APP_ID = "a3ee2cb6";
+  const YOUR_APP_KEY ="e7d541aa39bfccacbe7114164cb3ee03";
+
+  useEffect(() => {
+    fetch(
+      `https://api.edamam.com/search?q=${search}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=30&calories=591-722&health=alcohol-free`
+    )
+      .then((Response) => Response.json())
+      .then((data) => {
+        console.log(data.hits);
+        const newdata = data.hits;
+        setData(newdata);
+      });
+  }, [clicked]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <h1  style={{position:'relative',left:'500px'}}>food recipie app</h1>
+      <div   style={{position:'relative',left:'500px' ,top:"20px"}}>
+        <input style={{width:"300px",height:'20px'}}
+          type="text"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+         
+        ></input>
+        <button
+          onClick={() => {
+            setclicked((ps) => !ps);
+          }} >
+          Search
+        </button>
+        </div>
+        {data.length>=1 ? <Products  data={data}/>:null}   
+       
+       
+     
+     
+    
+      </div>
+      
+      
+    
+    
+)}
+;
 
 export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Product.css"
+const Products = ({data}) => {
+    return (
+        <div >
+            <div>
+                {data.map(data =>  
+                <div >
+                    <div  className='cards'>
+                    <img  src={data.recipe.image} alt="Card image cap" />
+                    <div class="card-body">
+                    <center>
+                        <h5>{data.recipe.label}</h5>
+                        <p>all about calaries {Math.round(data.recipe.calories)}</p>
+                        <button className='btn'>Buy</button>
+                        </center>
+                    </div>
+                    </div>
+                </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Products
